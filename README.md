96841ad190ebbe86e80aae6ecf11fc3766841818
reverite <samantha@chalker.io>
2018-04-03 13:25:58 -0700

    reshuffle 2018-04-03

---
+++ README.md
@@ -0,0 +1,7 @@
+# Plural.Cafe
+
+This is all of the files used for general utilities toward Plural Cafe.
+
+Files that are used on the main website (and if you want to make your test instance) are located in the "deploy" directory.
+
+Use at your own risk!
--- deploy/setup-mastodon.sh
+++ deploy/setup-mastodon.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-[ -z "$YML_LOC" ] && YML_LOC="$HOME"
+[ -z "$YML_LOC" ] && YML_LOC="$(pwd)"
 cd $YML_LOC
 
 echo "Setting up the instance..."
 echo
+mkdir -p "$YML_LOC/.docker/mastodon"
 
 curl -fsSL https://raw.githubusercontent.com/tootsuite/mastodon/master/.env.production.sample -o "$YML_LOC/.docker/mastodon/.env.production"
 
 MUID="$(docker-compose run --rm mstweb id -u 2>/dev/null)"
+MGID="$(docker-compose run --rm mstweb id -g 2>/dev/null)"
 SECRET_KEY_BASE=$(hexdump -vn 64 -e ' /1 "%02x"' /dev/urandom)
 OTP_SECRET=$(hexdump -vn 64 -e ' /1 "%02x"' /dev/urandom)
 
@@ -18,7 +20,7 @@ sed -i 's|ES_HOST=es|ES_HOST=mstes|' $YML_LOC/.docker/mastodon/.env.production
 sed -i "s|SECRET_KEY_BASE=|SECRET_KEY_BASE=$SECRET_KEY_BASE|" $YML_LOC/.docker/mastodon/.env.production
 sed -i "s|OTP_SECRET=|OTP_SECRET=$OTP_SECRET|" $YML_LOC/.docker/mastodon/.env.production
 sed -i "s|# UID=1000|UID=$MUID|" $YML_LOC/.docker/mastodon/.env.production
-sed -i "s|# GID=1000|GID=$MUID|" $YML_LOC/.docker/mastodon/.env.production
+sed -i "s|# GID=1000|GID=$MGID|" $YML_LOC/.docker/mastodon/.env.production
 
 docker-compose run --rm mstweb rake db:migrate
 (openssl dhparam -rand /dev/urandom -out $YML_LOC/.docker/nginx/dhparam.pem 4096 2>&1 >/dev/null) & pid=$!
@@ -27,3 +29,9 @@ echo
 echo "Mostly set up. Modify .docker/mastodon/.env.production settings and then"
 echo "you can do a 'docker-compose up -d' on this instance. OpenSSL is still"
 echo "running, so wait a bit for it to finish too."
+echo
+echo "There is an Nginx configuration file in conf/nginx.conf you can use."
+echo
+echo "Also, when you're going to the instance, register and then run this command:"
+echo "docker-compose run --rm mstweb rake mastodon:make_admin USERNAME=yourusername"
+echo
