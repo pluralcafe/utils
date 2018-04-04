342dada8e4aaa583fd389dcb34de915bd4d7c8b1
reverite <samantha@chalker.io>
2018-04-04 14:53:01 -0700

    container update script

--- deploy/scripts/update-containers.sh
+++ deploy/scripts/update-containers.sh
@@ -3,10 +3,17 @@
 [ -z "$COMPOSE" ] && COMPOSE="$(command -v docker-compose)"
 [ -z "$COMPOSE" ] && COMPOSE="/usr/local/bin/docker-compose"
 
-cd $HOME
+cd "$HOME" || exit
 
-($COMPOSE pull 2>&1 | grep --silent "Downloaded newer") && \
-  $COMPOSE up -d && \
-  docker cp $($COMPOSE ps -q mstweb):/mastodon/public/assets public/ && \
-  docker cp $($COMPOSE ps -q mstweb):/mastodon/public/packs public/ && \
+[ -z "$NGINX_WEBHOOK" ] || printf "Content-Type: text/plain\r\n\r\n"
+
+($COMPOSE pull 2>&1 | grep --silent "Downloaded newer") && {
+  $COMPOSE up -d
+
+  docker cp "$($COMPOSE ps -q mstweb):/mastodon/public/assets" public/
+  docker cp "$($COMPOSE ps -q mstweb):/mastodon/public/packs" public/
+
   docker image prune -f
+
+  curl -sS "https://raw.githubusercontent.com/pluralcafe/utils/master/deploy/docker-compose.yml" > docker-compose.yml
+}
